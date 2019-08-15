18b89c40650b0bf120628995d989f3679e9721b7
rwoelfle <rwoelfle@gmail.com>
2019-08-15 08:55:47 +0200

    removed JS redundancy

--- js/script.js
+++ js/script.js
@@ -8,14 +8,6 @@ $(document).ready(function() {
 	});
 
 	//Clicking navbar links should make the page scroll to the appropriate position rather than jump there
-	$("#navigation li a").click(function(e) {
-		e.preventDefault();
-
-		var targetElement = $(this).attr("href");
-		var targetPosition = $(targetElement).offset().top;
-		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow")
-	});
-
 	$("#navigation a").click(function(e) {
 		e.preventDefault();
 
